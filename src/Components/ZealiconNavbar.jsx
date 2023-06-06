fa6583183210bd123744f4a3894ecedf235720f4
Amish Verma <amishhverma@gmail.com>
2023-06-07 01:09:02 +0530

    Hopefully zealicon navbar is fixed

--- src/Components/ZealiconNavbar.jsx
+++ src/Components/ZealiconNavbar.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
-
+import styles from "../styles/zealicon.module.css"; 
 export default function ZealiconNavbar() {
   return (
-    <nav className="bg-white text-black flex justify-between py-6 px-4 sm:px-12">
-      <img src="/logo.svg" alt="" className="w-[7rem] h-[2rem] sm:w-[9rem] sm:h-[3rem]"></img>
-      <Link className="flex items-center justify-center border border-black rounded-2xl p-2 sm:px-4 sm:rounded-3xl" to="/">
-        <p className="text-xs sm:text-sm">www.jssquanta.com</p>
+    <nav className={styles.nav}>
+      <img src="/logo.svg" alt="" className= {styles.logo}></img>
+      <Link className={styles.link} to="/">
+        <p className={styles.site}>www.jssquanta.com</p>
         <img
           src="/arrow_up_right.svg"
           alt=""
-          className="w-[12px] h-[12px]"
+          className={styles.arrow}
         ></img>
       </Link>
     </nav>
