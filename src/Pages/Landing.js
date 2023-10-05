548fdf4eea4d47a85b2e542c28b8774a2f7a644f
Amish Verma <amishhverma@gmail.com>
2023-10-05 18:55:24 +0530

    fix: added vc details in landing page slider

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,33 +1,32 @@
-import About from "../Components/About";
-import Contact from "../Components/Contact";
-import Department from "../Components/Department";
-import Hero from "../Components/Hero";
-import Slider from "../Components/Slider";
-import { team_src, events_src } from "../Components/slider_src";
-import Navbar from "../Components/Navbar";
-import Footer from "../Components/Footer";
+import About from '../Components/About';
+import Contact from '../Components/Contact';
+import Hero from '../Components/Hero';
+import Slider from '../Components/Slider';
+import { team_src, events_src } from '../Components/slider_src';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
 
 const Landing = () => {
-  return (
-    <>
-      <Navbar />
-      <div style={{ overflowX: "hidden" }}>
-        <Hero
-          bgImg={"/assets/homepage-hero-img.png"}
-          heading={"Guide to Technology..."}
-          subHeading={"We LEARN, EXPLORE and GROW Together"}
-          bgPos={"95% -200%"}
-          bgSize={"1000px"}
-        />
-        <About />
-        <Slider src={team_src} sliderTitle={"Team Leads"} />
-        {/* <Department /> */}
-        <Slider src={events_src} sliderTitle={"Events"} />
-        <Contact />
-      </div>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<div style={{ overflowX: 'hidden' }}>
+				<Hero
+					bgImg={'/assets/homepage-hero-img.png'}
+					heading={'Guide to Technology...'}
+					subHeading={'We LEARN, EXPLORE and GROW Together'}
+					bgPos={'95% -200%'}
+					bgSize={'1000px'}
+				/>
+				<About />
+				<Slider src={team_src} sliderTitle={'Team Leads'} />
+				{/* <Department /> */}
+				<Slider src={events_src} sliderTitle={'Events'} />
+				<Contact />
+			</div>
+			<Footer />
+		</>
+	);
 };
 
 export default Landing;
